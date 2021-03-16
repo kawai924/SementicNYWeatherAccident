15b2a5dfc0755902f63788cf35faa173d39d5b7f
Andy <andisaplacan@me.com>
2021-03-16 15:12:49 -0700

    test

--- NY_data_extraction.py
+++ NY_data_extraction.py
@@ -34,7 +34,6 @@ def NYdata():
 
     # print(NYalldata)
     # filter out the certain type
-    # changed: test to see if i can push to master
     targetlist = ['TAVG','AWND', 'WESF']
     final = NYalldata[NYalldata["datatype"].isin(targetlist)]
     # print(NYalldata[NYalldata[2].isin(targetlist)])
